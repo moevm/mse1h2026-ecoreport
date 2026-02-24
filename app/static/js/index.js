13c94c7a72ac7f03e7f78d44021def5c31b3010a
OlegZhuchkov3343 <OlegZhuchkov2005@yandex.ru>
2026-02-25 00:50:16 +0300

    Форма для ввода данных

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,6 +1,6 @@
 async function uploadFile() {
     const fileInput = document.getElementById("file-input");
-    const status = document.getElementById("status");
+    const status = document.getElementById("upload-status");
 
     if (!fileInput.files.length) {
         status.innerText = "Выберите файл!";
@@ -22,10 +22,6 @@ async function uploadFile() {
 
         if (result.status === "success") {
             status.innerText = "Файл успешно загружен";
-            const form = document.getElementById("manual-input");
-            const input = document.getElementById('filename-input');
-            input.value = result.saved_as;
-            form.appendChild(input);
             console.log(result.saved_as);
 
         } else {
